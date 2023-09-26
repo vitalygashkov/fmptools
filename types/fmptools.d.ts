ea60df9112e02959b19eb9d48a8a8b1fcfd1f251
Vitaly Gashkov <vitalygashkov@vk.com>
2023-09-26 14:47:43 +0500

    fix: improve types

--- types/fmptools.d.ts
+++ types/fmptools.d.ts
@@ -1,3 +1,8 @@
+interface Request {
+  data: any;
+  url: string;
+}
+
 interface Response {
   data: any;
   request: {
@@ -6,16 +11,38 @@ interface Response {
 }
 
 /**
- * Create response interceptor to modify response data
+ * Create request interceptor to convert request data
  * @param schema - FMP schema
  * @returns interceptor function which can be used in `axios`, for example
  */
-export function createInterceptor(
+export function createRequestInterceptor(
+  schema: Record<string, unknown>
+): (value: Request) => Request | Promise<Request>;
+
+/**
+ * Create response interceptor to convert response data
+ * @param schema - FMP schema
+ * @returns interceptor function which can be used in `axios`, for example
+ */
+export function createResponseInterceptor(
   schema: Record<string, unknown>
 ): (value: Response) => Response | Promise<Response>;
 
 /**
- * Parse response data to human readable object
+ * Convert request data to FMP format (table entities as arrays)
+ * @param data - HTTP request payload
+ * @param schema - FMP schema
+ * @param resource - FMP resource name (for example: `ZFM_USERDATA`)
+ * @returns parsed data
+ */
+export function convertRequestData<T>(
+  data: any,
+  schema: Record<string, unknown>,
+  resource: string
+): T;
+
+/**
+ * Convert response data to human readable object
  * @param data - data from HTTP response
  * @param schema - FMP schema
  * @param resource - FMP resource name (for example: `ZFM_USERDATA`)
