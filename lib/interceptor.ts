b8cc7e07ff495b210132b891781f879b02e44a03
pitaev.ev <evgeny.pitaev@krit.pro>
2025-03-01 09:55:09 +0500

    add types for interceptor methods

--- lib/interceptor.ts
+++ lib/interceptor.ts
@@ -1,23 +1,26 @@
 import { convertRequestData, convertResponseData } from './converter';
+import { SchemaType } from '../types/SchemaType';
+import { Request, Response } from '../types/InterceptorTypes';
 
-const createResponseInterceptor = (schema) => {
-  return (response) => {
-    const url = response.request?.url || response.request?.responseURL || response.url;
+const createResponseInterceptor = (schema: SchemaType) => {
+  return (response: Response) => {
+    const url =
+      response.request?.url || response.request?.responseURL || response.url;
     const isRpcResource = url.includes('/rpc/');
     if (isRpcResource) {
-      const resource = url.split('/').at(-2);
+      const resource = url.split('/').at(-2) ?? '';
       response.data = convertResponseData(response.data, schema, resource);
     }
     return response;
   };
 };
 
-const createRequestInterceptor = (schema) => {
-  return (request) => {
+const createRequestInterceptor = (schema: SchemaType) => {
+  return (request: Request) => {
     const { url, data } = request;
     const isRpcResource = url.includes('/rpc/');
     if (isRpcResource) {
-      const resource = url.split('/').at(-2);
+      const resource = url.split('/').at(-2) ?? '';
       request.data = convertRequestData(data, schema, resource);
     }
     return request;
